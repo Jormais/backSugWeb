6489644bf90324faaec0f067c3e685dd0105c118
Jormillo <jonaybrito15@gmail.com>
2021-08-20 20:42:06 +0200

    add fetch and fetch one funcionalityincomplete, a bit part funcionality about contro users

--- api/users/users.controller.js
+++ api/users/users.controller.js
@@ -1,9 +1,47 @@
-function getUsers(req, res){
-    return res.json({name: "jonay", apellido : "Brito"});
-}
-function getOneUser(req, res){
+var mysql = require('mysql');
+var conexion = mysql.createPool({
+    host : 'direccion',
+    database : 'database',
+    user : 'usuario',
+    password : 'constraseña',
+});
 
+function getUsers(req, res){
+    conexion.getConnection(function(err) {
+        if (err) {
+            console.error('Error de conexion: ' + err.stack);
+            return;
+        }
+
+        conexion.query('SELECT * FROM usuarios', function (error, results, fields) {
+            if (error){
+                throw error;
+            }else {
+                return res.json(results);
+            }
+        });
+
+    });
 }
+
+function getOneUser(req, res){
+    conexion.getConnection(function(err) {
+        if (err) {
+            console.error('Error de conexion: ' + err.stack);
+            return;
+        }
+
+        conexion.query("SELECT * FROM usuarios WHERE usuarios.nombre='Katiuska'", function (error, results, fields) {
+            if (error){
+                throw error;
+            }else {
+                return res.json(results);
+            }
+        });
+
+    });
+}
+
 function deleteUSer(req, res){
 
 }
@@ -14,7 +52,22 @@ function patchOneUser(req, res){
 
 }
 function postOneUser(req, res){
+    conexion.getConnection(function(err) {
+        if (err) {
+            console.error('Error de conexion: ' + err.stack);
+            return;
+        }
 
+        conexion.query("INSERT INTO usuarios VALUES ('Nilsa','Brito Santana','1518','Otros','NilsaBrito@gmail.com','987654321','1','1','4');", function (error, results, fields) {
+            if (error){
+                throw error;
+            }else {
+                console.log("insert: " + results);
+                return res.json(results);
+            }
+        });
+
+    });
 }
 
 module.exports = {getUsers, getOneUser, deleteUSer, putUser, patchOneUser, postOneUser}
